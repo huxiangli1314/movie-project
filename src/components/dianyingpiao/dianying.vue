8fcd69c6407fd0add2dc68517a2aa3274de4df5a
897411890 <53297188+897411890@users.noreply.github.com>
2019-10-14 18:41:27 +0800

    10.14

--- src/components/dianyingpiao/dianying.vue
+++ src/components/dianyingpiao/dianying.vue
@@ -2,11 +2,12 @@
     <div>
         <h3>这里是"电影"项目列表</h3>
 
-<van-button type="default">电影</van-button>
-<van-button type="primary">电影</van-button>
-<van-button type="info">电影</van-button>
-<van-button type="warning">电影</van-button>
-<van-button type="danger">电影</van-button>
+
+
+
+
+
+
 
     </div>
 </template>
